6fbe5d11234a3bb30414b487aecaac7674c81b49
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-18 12:21:50 +0000

    [Add] assignment3 10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = User.find(@book.user.id)
   end
 
   private
