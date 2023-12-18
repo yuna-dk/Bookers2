ff77750b382caf8534cb9f3d848f32f212081cdd
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-18 05:14:35 +0000

    [Add] assignment3 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,24 @@ class UsersController < ApplicationController
     @users = User.all
     @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end
