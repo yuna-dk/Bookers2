f6977c4768f19bcdd9d0566e5e82115f457da922
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-18 08:59:52 +0000

    [Add] assignment3 9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book.id)
     else
       render :new
     end
@@ -16,10 +16,12 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
+    @book = Book.new()
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   private
