fd200c12a70b31857a9a62f1dd171d5db62e325b
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-19 02:39:38 +0000

    [Add] assignment3 11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,22 @@ class BooksController < ApplicationController
     @user = User.find(@book.user.id)
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'  
+  end
+
   private
 
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,14 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    @user.update(user_params_update)
     redirect_to user_path(@user.id)
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :profile_image)
+  def user_params_update
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def is_matching_login_user
