cb7364f57ee4446069b6c5e04716d0d1ba46395c
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-01-30 01:09:43 +0000

    [Add]guest_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :ensure_guest_user, only: [:edit]
 
   def new
     @user = User.new
@@ -59,5 +60,12 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
     end
   end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end  
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,18 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
+  GUEST_USER_EMAIL = "guest@example.com"
+
+    def self.guest
+      find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+      end
+    end
+
+    def guest_user?
+      email == GUEST_USER_EMAIL
+    end
+
 end
